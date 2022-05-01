4e12aca97f4f399b863e7d769bd8b7b3d85bc930
alejo4373 <alejandro@flowcode.com>
2022-05-01 11:45:36 -0400

    Start defining a theme and use it for buttons and inputs

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import store from './store';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from '@mui/material/styles'
+import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import store from './store'
+import theme from './theme'
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </BrowserRouter>,
   document.getElementById('root')
-);
+)
