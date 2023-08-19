1ca3bfbe47bd55b632df861728f389398bb02b6f
Alejandro Franco <alejandro@flowcode.com>
2023-08-19 18:41:24 -0400

    Update to React 18

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from '@mui/material/styles'
+import { createRoot } from 'react-dom/client'
 import App from './App'
 import React from 'react'
-import ReactDOM from 'react-dom'
 import store from './store'
 import theme from './theme'
 
-ReactDOM.render(
+const container = document.getElementById('app')
+const root = createRoot(container)
+root.render(
   <BrowserRouter>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
     </Provider>
-  </BrowserRouter>,
-  document.getElementById('root')
+  </BrowserRouter>
 )
